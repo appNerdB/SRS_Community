1c52bcad9c76bed9197beff4f35dd06ead30b7e3
Your Name <you@example.com>
2017-01-17 09:58:03 +0100

    Initial Commit

---
+++ SRS community/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SRS community
+//
+//  Created by Benny Franke Sounddesign on 17/01/17.
+//  Copyright © 2017 ShowRescueService. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
